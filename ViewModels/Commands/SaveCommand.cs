3cebdaefaba717144e08e0a21838959b8daa03b7
Vedran Isak <vedis613@student.liu.se>
2020-11-18 23:19:11 +0100

    Fixar merge

---
+++ ViewModels/Commands/SaveCommand.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDD49.ViewModels.Commands
+{
+    class SaveCommand
+    {
+    }
+}
